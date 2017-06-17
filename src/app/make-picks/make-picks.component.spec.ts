27b3684af310b6a8a5e31b3677a34020fe659b91
LocDaiNguyen <loc.nguyen.33@gmail.com>
2017-06-17 13:19:56 -0400

    setup test for make-picks component

--- src/app/make-picks/make-picks.component.spec.ts
+++ src/app/make-picks/make-picks.component.spec.ts
@@ -1,25 +1,95 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MockBackend } from '@angular/http/testing';
+import { XHRBackend } from '@angular/http';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-// import { MakePicksComponent } from './make-picks.component';
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+import 'rxjs/add/observable/forkJoin';
 
-// describe('MakePicksComponent', () => {
-//   let component: MakePicksComponent;
-//   let fixture: ComponentFixture<MakePicksComponent>;
+import { CoreModule } from '../core/core.module';
+import { SharedModule } from '../shared/shared.module';
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ MakePicksComponent ]
-//     })
-//     .compileComponents();
-//   }));
+import { SettingService } from '../core/services/setting.service';
+import { TeamService } from '../core/services/team.service';
+import { GameService } from '../core/services/game.service';
+import { PickService } from '../core/services/pick.service';
+import { CurrentWeekService } from '../core/services/current-week.service';
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(MakePicksComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+import { MakePicksComponent } from './make-picks.component';
+import { MatchupComponent } from './matchup/matchup.component';
+import { TeamComponent } from '../shared/team/team.component';
+import { ErrorValidationComponent } from '../shared/error-validation/error-validation.component';
+import { ErrorServerComponent } from '../shared/error-server/error-server.component';
+import { SuccessValidationComponent } from '../shared/success-validation/success-validation.component';
+import { NoDataComponent } from '../shared/no-data/no-data.component';
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+describe('MakePicksComponent', () => {
+  let component: MakePicksComponent;
+  let fixture: ComponentFixture<MakePicksComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        CommonModule,
+        HttpModule,
+        RouterModule,
+        ReactiveFormsModule,
+        RouterTestingModule,
+      ],
+      providers: [
+        SettingService,
+        TeamService,
+        GameService,
+        PickService,
+        CurrentWeekService,
+        {provide: XHRBackend, useClass: MockBackend}
+      ],
+      declarations: [
+        MakePicksComponent,
+        MatchupComponent,
+        TeamComponent,
+        ErrorValidationComponent,
+        ErrorServerComponent,
+        SuccessValidationComponent,
+        NoDataComponent
+      ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MakePicksComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should create', () => {
+    fixture.detectChanges();
+    expect(component).toBeTruthy();
+  });
+
+  // it('should load settings, teams, games, picks from server', () => {
+  //   let settingService = fixture.debugElement.injector.get('SettingService');
+  //   let teamService = fixture.debugElement.injector.get('TeamService');
+  //   let gameService = fixture.debugElement.injector.get('GameService');
+  //   let pickService = fixture.debugElement.injector.get('PickService');
+  //   spyOn(settingService, 'getAllSettings').and.returnValue(Observable.from([ {_id: 1} ]));
+  //   spyOn(teamService, 'getAllTeams').and.returnValue(Observable.from([ {_id: 1} ]));
+  //   spyOn(gameService, 'getAllGames').and.returnValue(Observable.from([ {_id: 1} ]));
+  //   spyOn(pickService, 'getAllPicks').and.returnValue(Observable.from([ {_id: 1} ]));
+    
+  //   fixture.detectChanges();
+
+  //   component.getAllData();
+
+  //   expect(component.teams.length).toBe(1)
+  // })
+});
